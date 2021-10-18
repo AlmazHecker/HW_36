4cee62b3ca904a71f6310d736f1cf7b88fd953d6
Almaz <Almazbek.sheraliev321@gmail.com>
2021-10-18 15:44:05 +0600

    mmm

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,24 @@
 import './Form.css'
 
 function Form(){
+
+
+
+
+    
     return(
-        <div></div>
+        <div>
+            <div>
+                <p>Name</p>
+                <input type='text' onChange={() => {}} value={}/>
+                
+                <p>text</p>
+                <input type='text' onChange={() => {}} value={}/>
+
+                <p>Date</p>
+                <input type='date' onChange={() => {}} value={}/>
+            </div>
+        </div>
     )
 }
 
