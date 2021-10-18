06a82655b20a17e17cd720893c58fcd8453a66fe
Almaz <Almazbek.sheraliev321@gmail.com>
2021-10-18 15:57:38 +0600

    save

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,50 @@
 import './Form.css'
+import React, { useState } from 'react'
 
 function Form(){
 
+    const [name, setName] = useState('')
+    const [text, setText] = useState('')
+    const [date, setDate] = useState('')
+
+    function NameChangeHandler(e){
+        setDate(e.target.value)
+    }
+    function TextChangeHandler(e){
+        setDate(e.target.value)
+    }
+    function DateChangeHandler(e){
+        setDate(e.target.value)
+    }
+
+    function Submitchik(e){
+        e.preventDefault();
+        let data = {
+            name: name,
+            text: text,
+            Date: new Date(date)
+        }
+
+        setDate('')
+        setName('')
+        setText('')
+
+    }
 
 
-
-    
     return(
-        <div>
+        <form onSubmit={() => {Submitchik()}}>
             <div>
                 <p>Name</p>
-                <input type='text' onChange={() => {}} value={}/>
+                <input type='text' onChange={() => {NameChangeHandler()}} value={name}/>
                 
                 <p>text</p>
-                <input type='text' onChange={() => {}} value={}/>
+                <input type='text' onChange={() => {TextChangeHandler()}} value={text}/>
 
                 <p>Date</p>
-                <input type='date' onChange={() => {}} value={}/>
+                <input type='date' onChange={() => {DateChangeHandler()}} value={date}/>
             </div>
-        </div>
+        </form>
     )
 }
 
